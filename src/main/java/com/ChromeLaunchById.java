8531f1fb0726c5ea761b089f094e7699be2e61f9
unknown <anju.bala1>
2021-09-02 18:03:09 +0530

    removed extra lines

--- src/main/java/com/ChromeLaunchById.java
+++ src/main/java/com/ChromeLaunchById.java
@@ -8,18 +8,13 @@ public class ChromeLaunchById {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:/work/workspace/Sel/exefiles/chromedriver.exe");
-		WebDriver cd= new ChromeDriver();
-		cd.navigate().to("http://www.fb.com/"); 
-		//cd.navigate().to("http://www.fb.com/"); 
-		
+		WebDriver cd = new ChromeDriver();
+		cd.navigate().to("http://www.fb.com/");
 
 		cd.findElement(By.id("email")).sendKeys("kamboj.anjana2017@gmail.com");
 		cd.findElement(By.id("pass")).sendKeys("anju.bala");
 		cd.findElement(By.cssSelector("div#u_0_d_zw")).click();
-		//cd.findElement(By.cssSelector("div#u_0_d_zw")).click();
-//123
-		//abc
-		
+
 		cd.findElement(By.className("a8c37x1j")).click();
 	}
 
