fd1a5ff8eb10a90ee67b439171634dbc48e43b30
HP <anju.bala1>
2021-09-14 10:53:23 +0530

    commits

--- src/main/java/pkg/Reverse.java
+++ src/main/java/pkg/Reverse.java
@@ -2,14 +2,14 @@ package pkg;
 
 public class Reverse {
 
-	public static void main(String[] args) {
-		String s="anjanA";
-		String p="";
-		for(int i=length()-1;i>=0;i--) {
-			p=p+s.charAt(i);
-			//System.out.println(i +" ---i---p--"+ p);
-		}
-		System.out.println(p);
-	}
+//	public static void main(String[] args) {
+//		String s="anjanA";
+//		String p="";
+//		for(int i=length()-1;i>=0;i--) {
+//			p=p+s.charAt(i);
+//			//System.out.println(i +" ---i---p--"+ p);
+//		}
+//		System.out.println(p);
+//	}
 
 }
